b35cdc32780535f2e4f24d762d552bd7a34d9e6d
Ansh <srivastava761@gmail.com>
2021-10-11 17:02:39 +0530

    Added files on 11 Oct 2021

---
+++ NPTEL_Programming_IN_Java/src/main/java/vid_lec/Eleventh_Oct.java
@@ -0,0 +1,22 @@
+
+
+package vid_lec;
+import java.util.Scanner;
+
+public class Eleventh_Oct {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter First No = ");
+		int num1, num2;
+		num1=s.nextInt();
+		System.out.println("Enter Second No = ");
+		num2=s.nextInt();
+		System.out.println("Sum of Numbers is = "+(num1+num2));
+		
+
+		
+
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# NPTEL-Programming_in_Java
-A course by IIT-Kharagpur, on the platform of Swayam.
